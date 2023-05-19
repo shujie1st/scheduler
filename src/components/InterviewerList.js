a3a62a1e277fc7d6081ba1869f16f957f164185c
Shujie Ma <shujie1st@gmail.com>
2023-05-19 16:54:11 -0400

    Update the names of props

--- src/components/InterviewerList.js
+++ src/components/InterviewerList.js
@@ -9,8 +9,8 @@ export default function InterviewerList(props) {
         key={interviewerItem.id}
         name={interviewerItem.name}
         avatar={interviewerItem.avatar}
-        selected={interviewerItem.id === props.interviewer}
-        setInterviewer={() => props.setInterviewer(interviewerItem.id)}
+        selected={interviewerItem.id === props.value}
+        setInterviewer={() => props.onChange(interviewerItem.id)}
       />
     );
   });
