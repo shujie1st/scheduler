77744fa94dab376a37c6b29c3b130c7e08ac19a4
Shujie Ma <shujie1st@gmail.com>
2023-05-28 19:51:43 -0400

    Implement getInterviewersForDay selector

--- src/components/Application.js
+++ src/components/Application.js
@@ -5,7 +5,7 @@ import "components/Application.scss";
 
 import DayList from "./DayList";
 import Appointment from "./Appointment";
-import { getAppointmentsForDay, getInterview } from "helpers/selectors";
+import { getAppointmentsForDay, getInterviewersForDay, getInterview } from "helpers/selectors";
 
 export default function Application(props) {
   const [state, setState] = useState({
@@ -28,16 +28,18 @@ export default function Application(props) {
   }, []);
 
   const dailyAppointments = getAppointmentsForDay(state, state.day);
+  const interviewers = getInterviewersForDay(state, state.day);
 
   const appointmentItems = dailyAppointments.map((appointment) => {
     const interview = getInterview(state, appointment.interview);
-
+    
     return (
       <Appointment
         key={appointment.id}
         id={appointment.id}
         time={appointment.time}
-        interview={interview} 
+        interview={interview}
+        interviewers={interviewers}
       />
     );
   });
--- src/helpers/selectors.js
+++ src/helpers/selectors.js
@@ -9,6 +9,17 @@ export function getAppointmentsForDay(state, day) {
   return apptsArray;
 }
 
+export function getInterviewersForDay(state, day) {
+  let interviewersArray = [];
+  let selectedDay = state.days.find(dayObject => dayObject.name === day);
+  if (selectedDay) {
+    for (const id of selectedDay.interviewers) {
+      interviewersArray.push(state.interviewers[id]);
+    }
+  }
+  return interviewersArray;
+}
+
 export function getInterview(state, interview) {
   if (interview && interview.interviewer) {
     return {...interview, interviewer: state.interviewers[interview.interviewer]};
