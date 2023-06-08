ecc871ea05f14e8669bad33614b4194f0f8d4f0f
Shujie Ma <shujie1st@gmail.com>
2023-06-07 20:59:10 -0400

    Validate props using prop-types

--- src/components/InterviewerList.js
+++ src/components/InterviewerList.js
@@ -1,4 +1,5 @@
 import React from "react";
+import PropTypes from "prop-types";
 import InterviewerListItem from "./InterviewerListItem";
 import "components/InterviewerList.scss"
 
@@ -21,4 +22,8 @@ export default function InterviewerList(props) {
       <ul className="interviewers__list">{interviewerItems}</ul>
     </section>
   );
+};
+
+InterviewerList.propTypes = {
+  interviewers: PropTypes.array.isRequired
 };
